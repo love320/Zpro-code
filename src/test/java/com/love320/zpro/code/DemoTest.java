67e48f3893e20ca0be042d4e88efd01f214cab0e
Love320 <admin@love320.com>
2013-12-18 17:19:43 +0800

    (添加)制作简易demo

--- src/test/java/com/love320/zpro/code/DemoTest.java
+++ src/test/java/com/love320/zpro/code/DemoTest.java
@@ -1,5 +1,7 @@
 package com.love320.zpro.code;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import org.springframework.beans.factory.BeanFactory;
 
 import com.love320.zpro.code.services.UserService;
@@ -8,9 +10,36 @@ import junit.framework.TestCase;
 
 public class DemoTest extends TestCase {
 	
+	private boolean isserver = false;
+	
+	private void runstop(){
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void runH2(){
+		if(isserver) return;
+		H2Server h2server = new H2Server();
+		try {
+			h2server.runTool(null);
+			isserver = true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public void testAppSpring(){
+		runH2();//
+		
 		BeanFactory srping = Spring.getSpring();
 		System.out.println(srping.getBean(UserService.class));
+		
+		//runstop();//
 	}
+	
 	
 }
